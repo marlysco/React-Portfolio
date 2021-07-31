6b76c8c963bb83958ebeb24f06ff962266a1be8a
marlysco <marlysco94@gmail.com>
2021-07-31 01:24:49 -0400

    Adding and customizing social media icons to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+
 export default function Footer() {
     return (
-  <div className={"footer bg-dark py-3 mt-auto text-light text-center"}>
-    This is the bloody footer
+  <div className={"footer bg-dark py-1 text-light text-center"}>
+    <a href="https://github.com/marlysco" className={"fa fa-linkedin icons"}></a>
+    <a href="https://www.linkedin.com/in/marlys-clemente-a922b118b/" className={"fa fa-github icons"}></a>
   </div>
     )
 }
